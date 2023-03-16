522062f00bd85e60bfe4400aafb72f910cff2bda
fifteenmania <ktthee1995@gmail.com>
2023-03-16 23:59:18 +0900

    feat: add motion

--- src/main.js
+++ src/main.js
@@ -1,43 +1,40 @@
-import * as Three from 'three';
+import { WebGLRenderer, sRGBEncoding, Scene, DirectionalLight, PerspectiveCamera, Group, GridHelper, Clock, AxesHelper } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { VRMLoaderPlugin, VRMUtils } from '@pixiv/three-vrm';
 import AvatarSample from '../assets/model/AvatarSample_B.vrm';
-import MotionSample from '../assets/fbx/Defeated.fbx';
+//import MotionSample from '../assets/fbx/Defeated.fbx';
 
-const renderer = new Three.WebGLRenderer();
-renderer.outputEncoding = Three.sRGBEncoding;
+const renderer = new WebGLRenderer();
+renderer.outputEncoding = sRGBEncoding;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const camera = new Three.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 2000 );
-camera.position.set(0, 1.6, -3);
+const camera = new PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 2000 );
+camera.position.set(0, 1.6, 6);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.screenSpacePanning = true;
-controls.target.set(0, 1.6, 0);
+controls.target.set(0, 1, 0);
 controls.update();
 
-const scene = new Three.Scene();
+const scene = new Scene();
 
-const light = new Three.DirectionalLight(0xffffff, 1);
-light.position.set(1, 1, 1).normalize();
+const light = new DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1).normalize();
 scene.add(light);
 
-const helperRoot = new Three.Group();
+const helperRoot = new Group();
 helperRoot.renderOrder = 10000;
 scene.add(helperRoot);
 
 let currentVrm = null;
-let currentMotion = null;
-
 const loader = new GLTFLoader();
 loader.crossOrigin = 'anonymous';
-loader.register(parser => new VRMLoaderPlugin(parser, {helperRoot}));
+loader.register(parser => new VRMLoaderPlugin(parser));
 loader.load(AvatarSample, gltf => {
     const vrm = gltf.userData.vrm;
-    currentVrm = vrm;
     VRMUtils.removeUnnecessaryVertices(gltf.scene);
     VRMUtils.removeUnnecessaryJoints(gltf.scene);
     VRMUtils.rotateVRM0(vrm);
@@ -45,6 +42,7 @@ loader.load(AvatarSample, gltf => {
       obj.frustumCulled = false;
     })
     scene.add(vrm.scene);
+    currentVrm = vrm;
     console.log(vrm);
   },
   (progress) => {
@@ -54,20 +52,30 @@ loader.load(AvatarSample, gltf => {
     console.log('An error happened', error);
 });
 
-const mixer = new Three.AnimationMixer(currentVrm.scene);
 
-
-const gridHelper = new Three.GridHelper(10, 10);
+const gridHelper = new GridHelper(10, 10);
 scene.add(gridHelper);
 
-const axesHelper = new Three.AxesHelper(5);
+const axesHelper = new AxesHelper(5);
 scene.add(axesHelper);
 
-const clock = new Three.Clock();
+const clock = new Clock();
 clock.start();
 
 function animate() {
   requestAnimationFrame(animate);
+  const deltaTime = clock.getDelta();
+  if (currentVrm) {
+    const amplitude = 0.15 * Math.PI;
+    const s = amplitude * Math.sin(Math.PI * 2 * clock.elapsedTime)
+    currentVrm.humanoid.getNormalizedBoneNode('neck').rotation.x = s;
+    currentVrm.humanoid.getNormalizedBoneNode('leftShoulder').rotation.z = s;
+    currentVrm.humanoid.getNormalizedBoneNode('rightShoulder').rotation.z = -s;
+    currentVrm.humanoid.getNormalizedBoneNode('hips').rotation.x = -s;
+    currentVrm.humanoid.getNormalizedBoneNode('leftUpperLeg').rotation.z = -s - amplitude;
+    currentVrm.humanoid.getNormalizedBoneNode('rightUpperLeg').rotation.z = s + amplitude;
+    currentVrm.update(deltaTime);
+  }
   renderer.render(scene, camera);
 }
 
